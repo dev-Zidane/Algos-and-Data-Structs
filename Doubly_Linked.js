0c380613bce28095356307ace300c9b8d48e7c26
Zidane Innis <zidaneinnis@gmail.com>
2023-08-03 21:33:40 -0500

    pop method added with comments

--- Doubly_Linked.js
+++ Doubly_Linked.js
@@ -12,4 +12,50 @@ class DoublyLinkedList {
 		this.tail = null;
 		this.length = 0;
 	}
+
+	push(val) {
+		let newNode = new Node(val);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.tail.next = newNode;
+			newNode.prev = this.tail;
+			this.tail = newNode;
+			this.length++;
+		}
+		return this;
+	}
+
+	pop() {
+		// Step 1: If the list is empty (no head), there's nothing to pop, so return undefined.
+		if (!this.head) return undefined;
+
+		// Step 2: Save the current tail node to return later.
+		let current = this.tail;
+
+		// Step 3: If there's only one node in the list.
+		if (this.length === 1) {
+			// Step 3.1: Set the head and the tail to be null (list is empty now).
+			this.head = null;
+			this.tail = null;
+
+			// Step 4: If there's more than one node in the list.
+		} else {
+			// Step 4.1: Update the tail to be the previous node.
+			this.tail = current.prev;
+
+			// Step 4.2: Set the new tail's next to null.
+			this.tail.next = null;
+
+			// Step 4.3: Disconnect the popped node from the list (clean up).
+			current.prev = null;
+		}
+
+		// Step 5: Decrease the length of the list by one.
+		this.length--;
+
+		// Step 6: Return the value of the node removed.
+		return current;
+	}
 }
